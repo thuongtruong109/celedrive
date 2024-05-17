a9ee8055f36b47e370d826dbc11f38abf0f809f8
truongtop1 <pattlactroi@gmail.com>
2024-05-17 12:18:34 +0700

    chore: update commitlint

--- apps/internal_single/server.js
+++ apps/internal_single/server.js
@@ -6,25 +6,25 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname+"/public")));
+app.use(express.static(path.join(__dirname + "/public")));
 
-io.on("connection", function(socket){
-	socket.on("sender-join",function(data){
-		socket.join(data.uid);
-	});
-	socket.on("receiver-join",function(data){
-		socket.join(data.uid);
-		socket.in(data.sender_uid).emit("init", data.uid);
-	});
-	socket.on("file-meta",function(data){
-		socket.in(data.uid).emit("fs-meta", data.metadata);
-	});
-	socket.on("fs-start",function(data){
-		socket.in(data.uid).emit("fs-share", {});
-	});
-	socket.on("file-raw",function(data){
-		socket.in(data.uid).emit("fs-share", data.buffer);
-	})
+io.on("connection", function(socket) {
+    socket.on("sender-join", function(data) {
+        socket.join(data.uid);
+    });
+    socket.on("receiver-join", function(data) {
+        socket.join(data.uid);
+        socket.in(data.sender_uid).emit("init", data.uid);
+    });
+    socket.on("file-meta", function(data) {
+        socket.in(data.uid).emit("fs-meta", data.metadata);
+    });
+    socket.on("fs-start", function(data) {
+        socket.in(data.uid).emit("fs-share", {});
+    });
+    socket.on("file-raw", function(data) {
+        socket.in(data.uid).emit("fs-share", data.buffer);
+    })
 });
 
 server.listen(5000);
