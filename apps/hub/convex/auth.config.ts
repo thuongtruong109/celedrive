009e74ccb8b959e2194bb04d441d42733a396f82
truongtop1 <pattlactroi@gmail.com>
2024-06-23 01:42:41 +0700

    chore(hub): change auth provider

--- apps/hub/convex/auth.config.ts
+++ apps/hub/convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://sharp-mutt-56.clerk.accounts.dev",
+      domain: "https://guiding-ghost-94.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
